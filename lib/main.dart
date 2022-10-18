ecc39b57902f168e0bc51b321be29b26b9a8c04e
RSoft456 <ramshamehmood456@gmail.com>
2022-10-19 03:04:51 +0500

    nai hua abi b

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart' as SF;
 
 void main() {
   runApp(const MyApp());
@@ -37,30 +38,45 @@ class _MyHomePageState extends State<MyHomePage> {
     //var file = "D:/FlutterProjects/pool_cover/assets/document.xlsx";
     //File doc = File(file);
     //var bytes = doc.readAsBytesSync();
-
-    log("{ ${file.lengthInBytes} } { ${file.offsetInBytes} } ");
+    //log("{ ${file.lengthInBytes} } { ${file.offsetInBytes} } ");
     var excel = Excel.decodeBytes(
         file.buffer.asUint8List(file.offsetInBytes, file.lengthInBytes));
     Sheet sheet = excel['Pricelist'];
-    var width = sheet.cell(CellIndex.indexByString("C4"));
-    var height = sheet.cell(CellIndex.indexByString("C8"));
-    width.value = Width;
-    height.value = height;
-    var result = sheet.cell(CellIndex.indexByString("F13"));
-    Formula price = result.value;
-    if (price.toString().isEmpty) {
-      log("yes");
-    }
-    log(price.toString());
-    log("value ${price.formula}");
-    //return price;
-    // for (var table in excel.tables["Pricelist"]) {
-    //   for (var element in table) {
-    //     if (element != null) {
-    //       log("${element.cellIndex}");
-    //     }
-    //   }
+
+    final SF.Workbook workbook = SF.Workbook();
+    final SF.Worksheet sheetObj = workbook.worksheets[0];
+
+    //sheet2.enableSheetCalculations();
+    // sheet2.getRangeByName('C4').setNumber(Height.toDouble());
+    // sheet2.getRangeByName('C8').setNumber(Width.toDouble());
+
+    // String? calculatedValue = sheet2.getRangeByName('IH13').calculatedValue;
+    // log("$calculatedValue");
+
+    // var width = sheet.cell(CellIndex.indexByString("C4"));
+    // var height = sheet.cell(CellIndex.indexByString("C8"));
+    // width.value = Width;
+    // height.value = height;
+    // var result = sheet.cell(CellIndex.indexByString("F13"));
+    // Formula price = result.value;
+    // if (price.toString().isEmpty) {
+    //   log("yes");
     // }
+    // log(price.toString());
+
+    // log("value ${price.formula}");
+    int count = 0;
+    for (var table in excel.tables["Calculator"]!.rows) {
+      sheetObj.importList(table, count, 1, true);
+      count++;
+      if (count == 13) {
+        Data fjk = table[1] as Data;
+        log("${fjk.cellIndex}    ${fjk.colIndex}    ${fjk.rowIndex}    ${fjk.value}");
+      }
+    }
+    var data = sheetObj.getRangeByName('H13:I13').calculatedValue;
+
+    log("$data");
   }
 
   @override
