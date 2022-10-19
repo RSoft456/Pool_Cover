c6072b0fbad063858e02b526b36c30fd5b341176
RSoft456 <ramshamehmood456@gmail.com>
2022-10-19 23:26:01 +0500

    19 october 22

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:syncfusion_flutter_xlsio/xlsio.dart' as SF;
 
 void main() {
   runApp(const MyApp());
@@ -35,55 +34,38 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   fileData(int Height, int Width) async {
     var file = await rootBundle.load("assets/document.xlsx");
-    //var file = "D:/FlutterProjects/pool_cover/assets/document.xlsx";
-    //File doc = File(file);
-    //var bytes = doc.readAsBytesSync();
-    //log("{ ${file.lengthInBytes} } { ${file.offsetInBytes} } ");
     var excel = Excel.decodeBytes(
         file.buffer.asUint8List(file.offsetInBytes, file.lengthInBytes));
-    Sheet sheet = excel['Pricelist'];
-
-    final SF.Workbook workbook = SF.Workbook();
-    final SF.Worksheet sheetObj = workbook.worksheets[0];
-
-    //sheet2.enableSheetCalculations();
-    // sheet2.getRangeByName('C4').setNumber(Height.toDouble());
-    // sheet2.getRangeByName('C8').setNumber(Width.toDouble());
-
-    // String? calculatedValue = sheet2.getRangeByName('IH13').calculatedValue;
-    // log("$calculatedValue");
-
-    // var width = sheet.cell(CellIndex.indexByString("C4"));
+    var worksheetsNames = excel.tables.keys;
+    // var sheet = excel.tables["Calculator"];
+    // var width = sheet!.cell(CellIndex.indexByString("C4"));
     // var height = sheet.cell(CellIndex.indexByString("C8"));
     // width.value = Width;
     // height.value = height;
-    // var result = sheet.cell(CellIndex.indexByString("F13"));
-    // Formula price = result.value;
-    // if (price.toString().isEmpty) {
-    //   log("yes");
-    // }
-    // log(price.toString());
-
-    // log("value ${price.formula}");
-    int count = 0;
-    for (var table in excel.tables["Calculator"]!.rows) {
-      sheetObj.importList(table, count, 1, true);
-      count++;
-      if (count == 13) {
-        Data fjk = table[1] as Data;
-        log("${fjk.cellIndex}    ${fjk.colIndex}    ${fjk.rowIndex}    ${fjk.value}");
+    for (var sheetName in worksheetsNames) {
+      if (sheetName == 'Calculator') {
+        var sheet = excel.tables[sheetName];
+        var sheetRows = sheet!.rows;
+        var rowsLength = sheetRows.length;
+        try {
+          for (var idx = 0; idx < rowsLength; idx++) {
+            var row = sheetRows[idx];
+            // print(" ${row.map((e) => '${e?.value}').join(' | ')}");
+            log("${row[0]?.value} | ${row[1]?.value} | ${row[2]?.value} | ${row[3]?.value} | ${row[4]?.value} | ${row[5]?.value} | ${row[6]?.value} | ${row[7]?.value} | ${row[8]?.value} ");
+          }
+        } catch (e) {
+          log("$e");
+        }
+        break;
       }
     }
-    var data = sheetObj.getRangeByName('H13:I13').calculatedValue;
-
-    log("$data");
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    fileData(8000, 9000);
+    fileData(5000, 4000);
   }
 
   @override
